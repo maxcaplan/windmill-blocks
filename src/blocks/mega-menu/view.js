7ec1ff31d1e5f069caa5947695e7121b341998cd
maxcaplan <maxacaplan@gmail.com>
2026-03-10 12:16:48 -0300

    Added focus management to mega menu interactivity

--- src/blocks/mega-menu/view.js
+++ src/blocks/mega-menu/view.js
@@ -1,12 +1,14 @@
 (() => {
-	/** @typedef {(e: Event) => void} ToggleCallback */
+	/** @typedef {(e: Event) => void} EventCallback */
 
 	/**
 	 * @typedef MegaMenu
-	 * @property {Element} element
-	 * @property {Element} menu
-	 * @property {Element} toggle
-	 * @property {ToggleCallback} toggleCallback
+	 * @property {HTMLElement} element
+	 * @property {HTMLElement} menu
+	 * @property {HTMLElement} toggle
+	 * @property {EventCallback} blurCallback
+	 * @property {EventCallback} toggleCallback
+	 * @property {EventCallback} menuCallback
 	 */
 
 	/** @type {MegaMenu[]} */
@@ -14,6 +16,66 @@
 
 	/** @type {MegaMenu|undefined} */
 	let open_menu = undefined;
+
+	//
+	// Functions
+	//
+
+	/**
+	 * Open a mega menu
+	 *
+	 * @param {MegaMenu} mega_menu
+	 */
+	const openMegaMenu = (mega_menu) => {
+		open_menu = mega_menu;
+		mega_menu.element.setAttribute('data-mega-menu-open', '');
+		mega_menu.toggle.setAttribute('aria-expanded', 'true');
+		mega_menu.menu.focus();
+	};
+
+	/**
+	 * Close a mega menu
+	 *
+	 * @param {MegaMenu} mega_menu
+	 */
+	const closeMegaMenu = (mega_menu) => {
+		open_menu = undefined;
+		mega_menu.element.removeAttribute('data-mega-menu-open');
+		mega_menu.toggle.setAttribute('aria-expanded', 'false');
+	};
+
+	//
+	// Event Handlers
+	//
+
+	/**
+	 * Mega menu blur event handler
+	 *
+	 * @param {FocusEvent} e
+	 * @param {number} menu_index
+	 * @param {MegaMenu[]} mega_menus
+	 */
+	const onMenuBlur = (e, menu_index, mega_menus) => {
+		const target_menu =
+			menu_index >= 0 && menu_index < mega_menus.length
+				? mega_menus[menu_index]
+				: undefined;
+
+		if (target_menu === undefined || open_menu !== target_menu) {
+			return;
+		}
+
+		if (e.relatedTarget === target_menu.toggle) {
+			return;
+		}
+
+		if (
+			!(e.relatedTarget instanceof Node) ||
+			!target_menu.menu.contains(e.relatedTarget)
+		) {
+			closeMegaMenu(target_menu);
+		}
+	};
 
 	/**
 	 * Mega menu toggle button click event handler
@@ -30,16 +92,45 @@
 				? mega_menus[menu_index]
 				: undefined;
 
+		// Close all other mega menus
 		mega_menus.forEach((menu) => {
 			if (target_menu === undefined || menu !== target_menu) {
 				menu.element.removeAttribute('data-mega-menu-open');
+				menu.toggle.setAttribute('aria-expanded', 'false');
 			}
 		});
 
-		if (target_menu?.element.toggleAttribute('data-mega-menu-open')) {
-			open_menu = target_menu;
+		if (target_menu === undefined) {
+			return;
+		}
+
+		// Toggle target mega menu
+		if (!target_menu.element.hasAttribute('data-mega-menu-open')) {
+			openMegaMenu(target_menu);
 		} else {
-			open_menu = undefined;
+			closeMegaMenu(target_menu);
+		}
+	};
+
+	/**
+	 * Mega menu menu click event handler
+	 *
+	 * @param {Event} e
+	 * @param {number} menu_index
+	 * @param {MegaMenu[]} mega_menus
+	 */
+	const onMenuMenuClick = (e, menu_index, mega_menus) => {
+		const target_menu =
+			menu_index >= 0 && menu_index < mega_menus.length
+				? mega_menus[menu_index]
+				: undefined;
+
+		if (target_menu === undefined || open_menu !== target_menu) {
+			return;
+		}
+
+		if (e.target === target_menu.menu) {
+			closeMegaMenu(target_menu);
 		}
 	};
 
@@ -50,27 +141,60 @@
 		document
 			.querySelectorAll('.wp-block-windmill-blocks-mega-menu')
 			.forEach((menu_element) => {
-				const toggle_element = menu_element.querySelector(
-					'.wp-block-windmill-blocks-mega-menu-toggle'
-				);
+				if (menu_element instanceof HTMLElement) {
+					const toggle_element = menu_element.querySelector(
+						'.wp-block-windmill-blocks-mega-menu-toggle-button'
+					);
 
-				const menu_menu_element = menu_element.querySelector(
-					'.wp-block-windmill-blocks-mega-menu-menu'
-				);
+					const menu_menu_element = menu_element.querySelector(
+						'.wp-block-windmill-blocks-mega-menu-menu'
+					);
 
-				if (toggle_element !== null && menu_menu_element !== null) {
-					const menu_index = mega_menus.length;
+					if (
+						toggle_element instanceof HTMLElement &&
+						menu_menu_element instanceof HTMLElement
+					) {
+						const menu_index = mega_menus.length;
 
-					const toggleCallback = (e) =>
-						onMenuToggleClick(e, menu_index, mega_menus);
-					toggle_element.addEventListener('click', toggleCallback);
+						// Create and assign menu blur callback
+						const blurCallback = (e) =>
+							onMenuBlur(e, menu_index, mega_menus);
+						menu_element.addEventListener(
+							'blur',
+							blurCallback,
+							true
+						);
 
-					mega_menus.push({
-						element: menu_element,
-						menu: menu_menu_element,
-						toggle: toggle_element,
-						toggleCallback,
-					});
+						// Create and assign menu toggle callback
+						const toggleCallback = (e) =>
+							onMenuToggleClick(e, menu_index, mega_menus);
+						toggle_element.addEventListener(
+							'click',
+							toggleCallback
+						);
+
+						// Create and assign menu menu click callback
+						const menuCallback = (e) =>
+							onMenuMenuClick(e, menu_index, mega_menus);
+						menu_menu_element.addEventListener(
+							'click',
+							menuCallback
+						);
+
+						// Ensure elements have proper attributes
+						toggle_element.setAttribute('aria-expanded', 'false');
+						menu_menu_element.setAttribute('tabindex', '-1');
+
+						// Create mega menu object
+						mega_menus.push({
+							element: menu_element,
+							menu: menu_menu_element,
+							toggle: toggle_element,
+							blurCallback,
+							toggleCallback,
+							menuCallback,
+						});
+					}
 				}
 			});
 	};
@@ -84,10 +208,13 @@
 		}
 
 		mega_menus.forEach((menu) => {
+			menu.element.removeEventListener('blur', menu.blurCallback);
 			menu.toggle.removeEventListener('click', menu.toggleCallback);
+			menu.menu.removeEventListener('click', menu.menuCallback);
 		});
 	};
 
+	// Attach to window load events
 	window.addEventListener('load', connectEventListeners);
 	window.addEventListener('beforeunload', disconnectEventListeners);
 })();
